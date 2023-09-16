df9863b8ad4155b7c8877e4b60860cd9baa81a55
Nick <nick@nowplus.org>
2023-09-16 21:31:08 +0800

    Added controller to control auth

---
+++ MessagingApp.Api/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using MessagingApp.Application.Commands;
+using MessagingApp.Application.DTOs;
+using MessagingApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessagingApp.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly ILogger<AuthController> _logger;
+    private readonly IMediator _mediator;
+    public AuthController(IMediator mediator, ILogger<AuthController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+    
+    [HttpPost]
+    public IActionResult RegisterUser(CreateUserDto createUserDto)
+    {
+        var command = new CreateUserCommand(createUserDto);
+        var result = _mediator.Send(command);
+        return Created("", result);
+    }
+}
