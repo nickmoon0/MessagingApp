bac9e5c5fff68ea1143f72be9c69601a0b88de3e
Nick <nick@nowplus.org>
2023-09-16 14:58:22 +0800

    Created ServiceCollection to register infrastructure

---
+++ MessagingApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Immutable;
+using MessagingApp.Application.Interfaces.Repositories;
+using MessagingApp.Infrastructure.Contexts;
+using MessagingApp.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Register contexts
+        var authConnString = configuration.GetConnectionString("AuthDb");
+        
+        services.AddDbContext<AuthContext>(opt =>
+            opt.UseMySql(authConnString, ServerVersion.AutoDetect(authConnString)));
+    
+        // Register repositories
+        services.AddScoped<IUserRepository, UserRepository>();
+        
+        return services;
+    }
+}
