032eec52f203be1242da5fa9b2df39b3af5513e1
Nick <nick@nowplus.org>
2023-09-16 19:03:23 +0800

    Ensured User table was correctly named

--- MessagingApp.Infrastructure/Contexts/AuthContext.cs
+++ MessagingApp.Infrastructure/Contexts/AuthContext.cs
@@ -8,4 +8,10 @@ public class AuthContext : DbContext
     public DbSet<User> Users { get; set; } = null!;
     
     public AuthContext(DbContextOptions<AuthContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        // Ensure table is called User and not Users
+        builder.Entity<User>(entity => entity.ToTable(nameof(User)));
+    }
 }
