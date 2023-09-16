27e897e36d31d9ff1d1fc1af7cb6759fd03119c3
Nick <nick@nowplus.org>
2023-09-16 21:27:35 +0800

    Added constructor to hash users passwords

--- MessagingApp.Domain/Entities/User.cs
+++ MessagingApp.Domain/Entities/User.cs
@@ -5,4 +5,11 @@ public class User
     public Guid Id { get; set; }
     public string? Username { get; set; }
     public string? HashedPassword { get; set; }
+    
+    public User() {}
+    public User(string username, string password)
+    {
+        Username = username;
+        HashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+    }
 }
