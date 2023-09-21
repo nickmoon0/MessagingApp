9ea9a31981608afc1f13bae966fb032d67ff200c
Nick <nick@nowplus.org>
2023-09-21 21:51:04 +0800

    Removed password from user as it is not required

--- MessagingApp.Domain/Entities/User.cs
+++ MessagingApp.Domain/Entities/User.cs
@@ -4,13 +4,11 @@ public class User
 {
     public Guid Id { get; set; }
     public string? Username { get; set; }
-    public string? Password { get; set; }
     
     public User() {}
 
-    public User(string? username, string? password)
+    public User(string? username)
     {
         Username = username;
-        Password = password;
     }
 }
