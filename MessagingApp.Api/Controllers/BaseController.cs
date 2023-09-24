d79dcc168aa22ed4edeec7a4c466d65e625b504a
Nick <nick@nowplus.org>
2023-09-24 11:27:28 +0800

    Created BaseController

---
+++ MessagingApp.Api/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessagingApp.Api.Controllers;
+
+public abstract class BaseController : ControllerBase
+{
+    protected string UserId => User.FindFirst(ClaimTypes.NameIdentifier)?.Value 
+                               ?? throw new AuthenticationException();
+    protected string GetClaimValue(string claimType) => User.FindFirst(claimType)?.Value 
+                                                        ?? throw new AuthenticationException();
+}
