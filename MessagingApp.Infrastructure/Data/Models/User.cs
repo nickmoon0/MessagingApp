ce588a6cd2c8dd873b48f7fb79764c7a03433b48
Nick <nick@nowplus.org>
2023-09-24 20:55:11 +0800

    Deleted infra models and migrations

--- MessagingApp.Infrastructure/Data/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MessagingApp.Infrastructure.Data.Models;
-
-public class User
-{
-    public Guid Id { get; set; }
-    public string Username { get; set; } = null!;
-    public List<UserFriend> Friends { get; set; } = null!;
-}
