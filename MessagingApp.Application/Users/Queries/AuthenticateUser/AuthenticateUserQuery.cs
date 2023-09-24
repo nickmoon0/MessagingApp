74759f1e0c081effc1d84fc7bc12b039c836b06d
Nick <nick@nowplus.org>
2023-09-24 22:46:40 +0800

    Made properties non-nullable as request has these values as required

--- MessagingApp.Application/Users/Queries/AuthenticateUser/AuthenticateUserQuery.cs
+++ MessagingApp.Application/Users/Queries/AuthenticateUser/AuthenticateUserQuery.cs
@@ -4,8 +4,8 @@ namespace MessagingApp.Application.Users.Queries.AuthenticateUser;
 
 public class AuthenticateUserQuery : IRequest<string>
 {
-    public string? Username { get; init; }
-    public string? Password { get; init; }
+    public string Username { get; init; }
+    public string Password { get; init; }
 
     public AuthenticateUserQuery(AuthenticateUserRequest authenticateUserRequest)
     {
