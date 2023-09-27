9878ca7d9f4b3cdb2c483e44c171cefc6b441723
Nick <nick@nowplus.org>
2023-09-27 17:16:25 +0800

    Created request and command for accepting friend requests

--- MessagingApp.Application/FriendRequests/Commands/AcceptFriendRequest/AcceptFriendRequestCommand.cs
+++ MessagingApp.Application/FriendRequests/Commands/AcceptFriendRequest/AcceptFriendRequestCommand.cs
@@ -4,5 +4,14 @@ namespace MessagingApp.Application.FriendRequests.Commands.AcceptFriendRequest;
 
 public class AcceptFriendRequestCommand : IRequest<AcceptFriendRequestResponse>
 {
-    
+    public Guid FriendRequestId { get; set; }
+    public Guid ToUserId { get; set; }
+    public Guid RequestingUserId { get; set; }
+
+    public AcceptFriendRequestCommand(AcceptFriendRequestRequest request, Guid requestingUserId)
+    {
+        FriendRequestId = request.FriendRequestId;
+        ToUserId = requestingUserId;
+        RequestingUserId = requestingUserId;
+    }
 }
---
+++ MessagingApp.Application/FriendRequests/Commands/AcceptFriendRequest/AcceptFriendRequestRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace MessagingApp.Application.FriendRequests.Commands.AcceptFriendRequest;
+
+public class AcceptFriendRequestRequest
+{
+    public Guid FriendRequestId { get; set; }
+}
