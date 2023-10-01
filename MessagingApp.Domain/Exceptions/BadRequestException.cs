a13442ceef5c5538af4e96395f4c839161661cc5
Nick <nick@nowplus.org>
2023-10-01 13:37:21 +0800

    Created domain exceptions

---
+++ MessagingApp.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,6 @@
+﻿namespace MessagingApp.Domain.Exceptions;
+
+public class BadRequestException : DomainException
+{
+    public BadRequestException(string message, string code) : base(message, code) { }
+}
---
+++ MessagingApp.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿namespace MessagingApp.Domain.Exceptions;
+
+public abstract class DomainException : Exception
+{
+    public string Code { get; private set; }
+
+    protected DomainException(string message, string code) : base(message)
+    {
+        Code = code;
+    }
+}
