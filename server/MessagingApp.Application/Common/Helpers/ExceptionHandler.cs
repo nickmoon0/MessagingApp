a696a0916dd3dfec045dba5eb20fdb7304744ce0
Nick <nick@nowplus.org>
2023-12-20 22:21:04 +0800

    Created ActionResult and ExceptionHandler to transport exceptions between domain and application layer

---
+++ server/MessagingApp.Application/Common/Helpers/ExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using MessagingApp.Application.Common.Exceptions;
+using MessagingApp.Domain.Exceptions;
+
+namespace MessagingApp.Application.Common.Helpers;
+
+public static class ExceptionHelper
+{
+    public static Exception ResolveException(DomainException ex)
+    {
+        return ex switch
+        {
+            BadRequestException badReq => new InvalidOperationException(badReq.Message),
+            NotFoundException notFound => new EntityNotFoundException(notFound.Message),
+            UnauthorisedException unAuth => new UnauthorizedAccessException(unAuth.Message),
+            _ => throw new Exception(ex.Message)
+        };
+    }
+}
---
+++ server/MessagingApp.Domain/Common/ActionResult.cs
@@ -0,0 +1,23 @@
+﻿using MessagingApp.Domain.Exceptions;
+
+namespace MessagingApp.Domain.Common;
+
+public class ActionResult<T>
+{
+    public bool Success { get; init; }
+
+    public DomainException? Error { get; init; }
+    public T? Result { get; init; }
+
+    public ActionResult(T obj)
+    {
+        Success = true;
+        Result = obj;
+    }
+    
+    public ActionResult(DomainException ex)
+    {
+        Success = false;
+        Error = ex;
+    }
+}
